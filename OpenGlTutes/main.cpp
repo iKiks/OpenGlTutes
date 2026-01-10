16fa2cd1339574b08109ec0114f0eb722558e1fc
iKiks <ikiks.lanre@gmail.com>
2026-01-10 22:25:07 +0100

    project update

--- OpenGlTutes/main.cpp
+++ OpenGlTutes/main.cpp
@@ -14,6 +14,8 @@ int main() {
 	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
 	glm::mat4 trans = glm::mat4(1.0f);
 	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	trans = glm::scale(trans, glm::vec3(2.0f, 2.0f, 2.0f));
 	vec = trans * vec;
 	std::cout << vec.x << ", " << vec.y << ", " << vec.z << std::endl;
 
