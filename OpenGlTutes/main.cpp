465f1fc7fcc25f099640375f235c65b02f17cf78
iKiks <ikiks.lanre@gmail.com>
2026-01-10 22:14:50 +0100

    project update

--- OpenGlTutes/main.cpp
+++ OpenGlTutes/main.cpp
@@ -13,6 +13,7 @@ int main() {
 	//GLM test
 	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
 	glm::mat4 trans = glm::mat4(1.0f);
+	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
 	vec = trans * vec;
 	std::cout << vec.x << ", " << vec.y << ", " << vec.z << std::endl;
 
